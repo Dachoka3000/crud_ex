686980a636f9e81a4b34f9b1c797205950996b09
Machoka Daisy <machokadaisy25@gmail.com>
2022-03-08 08:52:45 +0300

    add a forula to read data inputted by the user

---
+++ script.js
@@ -0,0 +1,10 @@
+var selectedRow = null
+
+function readFormData() {
+    var formData = {};
+    formData["productCode"] = document.getElementById("productCode").value;
+    formData["product"] = document.getElementById("product").value;
+    formData["qty"] = document.getElementById("qty").value;
+    formData["perPrice"] = document.getElementById("perPrice").value;
+    return formData;
+}
