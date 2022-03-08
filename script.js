187e92c0ad1da335d138e6146c53c8366ffff42b
Machoka Daisy <machokadaisy25@gmail.com>
2022-03-08 09:01:06 +0300

    add a function to delete data

--- script.js
+++ script.js
@@ -31,9 +31,18 @@ function onEdit(td) {
     document.getElementById("qty").value = selectedRow.cells[2].innerHTML;
     document.getElementById("perPrice").value = selectedRow.cells[3].innerHTML;
 }
+
 function updateRecord(formData) {
     selectedRow.cells[0].innerHTML = formData.productCode;
     selectedRow.cells[1].innerHTML = formData.product;
     selectedRow.cells[2].innerHTML = formData.qty;
     selectedRow.cells[3].innerHTML = formData.perPrice;
+}
+
+function onDelete(td) {
+    if (confirm('Do you want to delete this record?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById('storeList').deleteRow(row.rowIndex);
+        resetForm();
+    }
 }
