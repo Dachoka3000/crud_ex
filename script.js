0386b2121a8639aad8e09590d1e4bfc65a4b8d83
Machoka Daisy <machokadaisy25@gmail.com>
2022-03-08 08:55:17 +0300

    add a forula to save data to the table once inputted

--- script.js
+++ script.js
@@ -8,3 +8,18 @@ function readFormData() {
     formData["perPrice"] = document.getElementById("perPrice").value;
     return formData;
 }
+
+function insertNewRecord(data) {
+    var table = document.getElementById("storeList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+		cell1.innerHTML = data.productCode;
+    cell2 = newRow.insertCell(1);
+		cell2.innerHTML = data.product;
+    cell3 = newRow.insertCell(2);
+		cell3.innerHTML = data.qty;
+    cell4 = newRow.insertCell(3);
+		cell4.innerHTML = data.perPrice;
+    cell4 = newRow.insertCell(4);
+        cell4.innerHTML = `<button onClick="onEdit(this)">Edit</button> <button onClick="onDelete(this)">Delete</button>`;
+}
